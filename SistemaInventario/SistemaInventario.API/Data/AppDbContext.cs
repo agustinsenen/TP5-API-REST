1e1255377650e8dd8c6205441848bce3615b56c1
Agustin <agustinsenen1999@gmail.com>
2026-09-15 18:02:20 -0300

    Configurar Entity Framework Core y AppDbContext

--- SistemaInventario/SistemaInventario.API/Data/AppDbContext.cs
+++ SistemaInventario/SistemaInventario.API/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace SistemaInventario.API.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaInventario.API.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
     }
 }
